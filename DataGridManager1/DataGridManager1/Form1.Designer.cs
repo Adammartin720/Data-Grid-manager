d50f83eb04b4d45f6b5058b5dafc18ccb9f582a0
Adammartin720 <49869770+Adammartin720@users.noreply.github.com>
2019-04-29 12:41:53 +0100

    Add files via upload

---
+++ DataGridManager1/DataGridManager1/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace DataGridManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DGCustomer = new System.Windows.Forms.DataGridView();
+            this.DGCustomerID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DGCustName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DGCustMobile = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BtnLoad = new System.Windows.Forms.Button();
+            this.BtnCellValue = new System.Windows.Forms.Button();
+            this.BtnRow = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnPrint = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.DGCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGCustomer
+            // 
+            this.DGCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGCustomer.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DGCustomerID,
+            this.DGCustName,
+            this.DGCustMobile});
+            this.DGCustomer.Location = new System.Drawing.Point(29, 27);
+            this.DGCustomer.Name = "DGCustomer";
+            this.DGCustomer.Size = new System.Drawing.Size(545, 140);
+            this.DGCustomer.TabIndex = 0;
+            // 
+            // DGCustomerID
+            // 
+            this.DGCustomerID.HeaderText = "Customer ID";
+            this.DGCustomerID.Name = "DGCustomerID";
+            // 
+            // DGCustName
+            // 
+            this.DGCustName.HeaderText = "Name";
+            this.DGCustName.Name = "DGCustName";
+            this.DGCustName.Width = 300;
+            // 
+            // DGCustMobile
+            // 
+            this.DGCustMobile.HeaderText = "Mobile";
+            this.DGCustMobile.Name = "DGCustMobile";
+            // 
+            // BtnLoad
+            // 
+            this.BtnLoad.Location = new System.Drawing.Point(595, 27);
+            this.BtnLoad.Name = "BtnLoad";
+            this.BtnLoad.Size = new System.Drawing.Size(97, 23);
+            this.BtnLoad.TabIndex = 1;
+            this.BtnLoad.Text = "Load";
+            this.BtnLoad.UseVisualStyleBackColor = true;
+            this.BtnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // BtnCellValue
+            // 
+            this.BtnCellValue.Location = new System.Drawing.Point(595, 56);
+            this.BtnCellValue.Name = "BtnCellValue";
+            this.BtnCellValue.Size = new System.Drawing.Size(97, 23);
+            this.BtnCellValue.TabIndex = 2;
+            this.BtnCellValue.Text = "Get Cell Value";
+            this.BtnCellValue.UseVisualStyleBackColor = true;
+            this.BtnCellValue.Click += new System.EventHandler(this.BtnCellValue_Click);
+            // 
+            // BtnRow
+            // 
+            this.BtnRow.Location = new System.Drawing.Point(595, 86);
+            this.BtnRow.Name = "BtnRow";
+            this.BtnRow.Size = new System.Drawing.Size(97, 23);
+            this.BtnRow.TabIndex = 3;
+            this.BtnRow.Text = "Delete Row";
+            this.BtnRow.UseVisualStyleBackColor = true;
+            this.BtnRow.Click += new System.EventHandler(this.BtnRow_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(595, 116);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(97, 23);
+            this.BtnSave.TabIndex = 4;
+            this.BtnSave.Text = "Save Data";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // BtnPrint
+            // 
+            this.BtnPrint.Location = new System.Drawing.Point(595, 145);
+            this.BtnPrint.Name = "BtnPrint";
+            this.BtnPrint.Size = new System.Drawing.Size(97, 22);
+            this.BtnPrint.TabIndex = 5;
+            this.BtnPrint.Text = "Print";
+            this.BtnPrint.UseVisualStyleBackColor = true;
+            this.BtnPrint.Click += new System.EventHandler(this.BtnPrint_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "PPDReport";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.PDReport_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 229);
+            this.Controls.Add(this.BtnPrint);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.BtnRow);
+            this.Controls.Add(this.BtnCellValue);
+            this.Controls.Add(this.BtnLoad);
+            this.Controls.Add(this.DGCustomer);
+            this.Name = "Form1";
+            this.Text = "FrnDataEntry";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGCustomer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGCustomer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CustName;
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DGCustomerID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DGCustName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DGCustMobile;
+        private System.Windows.Forms.Button BtnCellValue;
+        private System.Windows.Forms.Button BtnRow;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnPrint;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ DataGridManager1/DataGridManager1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataGridManager
+{
+    public partial class Form1 : Form
+    {
+        string[] sCustID = new string[10];
+        string[] sCustName = new string[10];
+        string[] sCustMoble = new string[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sCustID[0] = "100"; sCustName[0] = "John Williams"; sCustMoble[0] = "01639 355434";
+            sCustID[1] = "101"; sCustName[1] = "Bill Smith";    sCustMoble[1] = "01639 352925";
+            sCustID[2] = "102"; sCustName[2] = "David Walliams"; sCustMoble[2] = "01639 678965";
+            sCustID[3] = "103"; sCustName[3] = "Jason Jones";    sCustMoble[3] = "01639 554433";
+            sCustID[4] = "104"; sCustName[4] = "Max Miller";     sCustMoble[4] = "01639 456343";
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+           DGCustomer.Rows.Clear();            
+           for (int i = 0; i <= sCustID.Length-1; i++)
+            {
+                if (sCustID[i] == null) break; 
+                DataGridViewRow row = (DataGridViewRow)DGCustomer.Rows[0].Clone();
+                row.Cells[0].Value = sCustID[i];
+                row.Cells[1].Value = sCustName[i];
+                row.Cells[2].Value = sCustMoble[i];
+                DGCustomer.Rows.Add(row);
+            }
+        }
+
+        private void BtnCellValue_Click(object sender, EventArgs e)
+        {
+            int Row = DGCustomer.SelectedCells[0].RowIndex;
+            int Collumn = DGCustomer.SelectedCells[0].ColumnIndex;
+            string Data = DGCustomer.Rows[Row].Cells[Collumn].Value.ToString ();
+            MessageBox.Show(Data);
+        }
+
+        private void BtnRow_Click(object sender, EventArgs e)
+        {
+            int Row = DGCustomer.SelectedCells[0].RowIndex;
+            DGCustomer.Rows.RemoveAt(Row);
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+
+            Array.Clear(sCustID, 0, sCustID.Length);
+            Array.Clear(sCustName, 0, sCustName.Length);
+            Array.Clear(sCustMoble, 0, sCustMoble.Length);
+
+            int Rcount = DGCustomer.Rows.Count;
+            for (int Row = 0; Row < Rcount-1; Row++)
+            {
+                sCustID[Row] = DGCustomer.Rows[Row].Cells[0].Value.ToString();
+                sCustName[Row] = DGCustomer.Rows[Row].Cells[1].Value.ToString();
+                sCustMoble[Row] = DGCustomer.Rows[Row].Cells[2].Value.ToString();
+            }
+        }
+
+        private void PDReport_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap image = new Bitmap("NPTC.png");                                
+            e.Graphics.DrawImage(image, new Rectangle(0, 0, 200, 200));
+            
+            int Col = 50;  int Row = 250;
+            Font MainHeading = new Font("courier New", 20, FontStyle.Bold);
+            Brush Blue =  Brushes.Blue;
+            e.Graphics.DrawString("Report", MainHeading, Blue, Col, Row);
+            
+            
+            Col = 50; Row= Row+50;
+            Font DataHeading = new Font("courier New", 12, FontStyle.Bold);
+            Brush Red =   Brushes.Red ;
+            e.Graphics.DrawString("Customer Id", DataHeading, Blue, Col, Row);
+            e.Graphics.DrawString("Name", DataHeading, Blue, Col + 200, Row);
+            e.Graphics.DrawString("Mobile", DataHeading, Blue, Col + 400, Row);
+            e.Graphics.DrawString("_________________________________________________________", DataHeading, Red, Col, Row);
+            
+            
+
+
+
+            Font TextData =    new Font("courier New", 12, FontStyle.Regular);  
+            Brush Black = Brushes.Black;
+            
+            for (int i = 0; i <= sCustID.Length - 1; i++)
+            {
+                Row = Row + 25;
+                if (sCustID[i] == null) break;
+                e.Graphics.DrawString(sCustID[i], TextData, Black, Col, Row);
+                e.Graphics.DrawString(sCustName[i], TextData, Black, Col + 200, Row);
+                e.Graphics.DrawString(sCustMoble[i], TextData, Black, Col + 400, Row);     
+            }
+
+            e.Graphics.DrawString("_________________________________________________________", DataHeading, Red, Col, Row);
+        }
+
+        private void BtnPrint_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+
+        }
+
+    }
+}
